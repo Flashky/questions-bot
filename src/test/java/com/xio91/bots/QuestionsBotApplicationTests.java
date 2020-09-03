8558675c88b02665f4de5a53e76753a45450e83d
Flashky <mevengar@gmail.com>
2020-09-03 17:31:11 +0200

    Created project structure

---
+++ src/test/java/com/xio91/bots/QuestionsBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xio91.bots;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestionsBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
