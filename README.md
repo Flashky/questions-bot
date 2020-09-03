a16040188f7dd483c710591cfeb22c5a147ebf48
Flashky <mevengar@gmail.com>
2020-09-03 18:52:43 +0200

    Added basic bot connection

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
 # questions-bot
 
+## Program arguments
+
+Temporary, while starting the development, the oauth token can be added via the first program argument with no "oauth:" prefix on it.
+
 ## Configuration properties to set
+
+You can set this configuration properties by overriding either ``application.yml`` or as environment variables.
 
 OAuth configuration:
 
@@ -12,3 +18,4 @@ IRC configuration:
 - twitch.irc.host: default value if not set irc.chat.twitch.tv
 - twitch.irc.nick: Bot nick
 - twitch.irc.channel: Channel to join without hashtag (#)
+
--- src/main/java/com/xio91/bots/QuestionsBotApplication.java
+++ src/main/java/com/xio91/bots/QuestionsBotApplication.java
@@ -1,21 +1,27 @@
 package com.xio91.bots;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.kitteh.irc.client.library.Client;
+import org.kitteh.irc.client.library.Client.Builder;
+import org.kitteh.irc.client.library.feature.twitch.TwitchSupport;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.xio91.bots.properties.TwitchIRCProperties;
-import com.xio91.bots.properties.TwitchOAuthProperties;
 
 @SpringBootApplication
 public class QuestionsBotApplication implements CommandLineRunner {
 
-	@Autowired
-	private TwitchIRCProperties ircProperties;
+	private final static String OAUTH_PREFIX = "oauth:";
+	private final static String CHANNEL_PREFIX = "#";
 	
 	@Autowired
-	private TwitchOAuthProperties oauthProperties;
+	private TwitchIRCProperties ircProperties;
+
 	
 	public static void main(String[] args) {
 		SpringApplication.run(QuestionsBotApplication.class, args);
@@ -23,9 +29,29 @@ public class QuestionsBotApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		// TODO Auto-generated method stub
-		System.out.println(ircProperties);
-		System.out.println(oauthProperties);
+		
+		// Connection configuration
+		Builder clientBuilder =  Client.builder()
+					.server()
+					.host(ircProperties.getHost())
+					.password(OAUTH_PREFIX + args[1])
+				.then()
+					.nick(ircProperties.getNick());
+		 
+		// Debug chat input/output
+		SimpleDateFormat sdf = new SimpleDateFormat("mm:ss");
+		clientBuilder.listeners().input(line -> System.out.println(sdf.format(new Date()) + ' ' + "[I] " + line));
+		clientBuilder.listeners().output(line -> System.out.println(sdf.format(new Date()) + ' ' + "[O] " + line));
+		clientBuilder.listeners().exception(Throwable::printStackTrace);
+		 
+		// Build the client and add twitch support
+		Client client = clientBuilder.build();
+		TwitchSupport.addSupport(client);
+
+		// Connect and joint to channel
+		client.connect();
+		client.addChannel(CHANNEL_PREFIX + ircProperties.getChannel());
+		
 	}
 
 }
