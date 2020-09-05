781e61dfe90dcb7b993c4c4cd0b6e33df84815cc
Flashky <mevengar@gmail.com>
2020-09-05 19:47:59 +0200

    Removed callback mapping and added root mapping

--- src/main/java/com/xio91/bots/controllers/CallbackController.java
+++ src/main/java/com/xio91/bots/controllers/CallbackController.java
@@ -1,7 +1,8 @@
-package com.xio91.bots.web.controllers;
+package com.xio91.bots.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.security.oauth2.client.OAuth2ClientProperties;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,17 +12,12 @@ public class CallbackController {
 	@Autowired
 	private OAuth2ClientProperties oauth2ClientProperties;
 	
-	@GetMapping("/callback")
-	public void callback() {
-		
-		System.out.println("Callback!!");
-		System.out.println(oauth2ClientProperties.getProvider().get("twitch").getTokenUri());
-		
-		// TODO:
-		// 1. Solicitar el token 
-		// 2. Iniciar el bot
-		
-		// Avanzado: Redireccionar a otra URL (authenticated restricted) en el que se pueda configurar y arrancar al bot.
+	@Autowired
+	private OAuth2AuthorizedClientService service; 
+	
+	@GetMapping("/")
+	public String root() {
+		return "root page";
 	}
 
 }
