c18c1a52ede38088fb5143d3c3a22680013296ba
Flashky <mevengar@gmail.com>
2020-09-06 16:29:57 +0200

    Implemented full OAuth 2.0 handshake chain
    At the end, the last problem to solve was to retrieve authenticated user credentials.
    Problem was that cookie was STATELESS. After removing STATELESS, it is now possible to retrieve authentication user details and tokens.

--- src/main/java/com/xio91/bots/config/SecurityConfig.java
+++ src/main/java/com/xio91/bots/config/SecurityConfig.java
@@ -1,9 +1,12 @@
 package com.xio91.bots.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 
 @Configuration
@@ -12,16 +15,46 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
     	
-        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-        .and()
-        	.authorizeRequests()
-        	.antMatchers("/", "/home", "/login**", "/webjars/**", "/error**", "/oauth2/authorization/**")
-        	.permitAll()
+    	// TODO fix "/" being an permitted access point.
+    	
+    	// Problem 1: The Spring Boot OAuth 2.0 handshake finish at "/".
+    	
+    	// If that endpoint is unauthorized, the handshake enters in a redirect loop:
+    	// http://localhost:8080/login 
+    	// 		-> https://id.twitch.tv/oauth2/authorize?...
+    	// 			-> http://localhost:8080/login/oauth2/code/
+    	//				-> https://id.twitch.tv/oauth2/token
+    	// 				-> https://id.twitch.tv/oauth2/userinfo
+    	//			-> http://localhost:8080/ -> Not allowed -> http://localhost:8080/login (repeat)
+    	
+        http.authorizeRequests()
+        	.antMatchers("/","/login**", 
+        			"/webjars/**",
+        			"/error**", 
+        			"/oauth2/authorization/**",
+        			"/oauth/code/*",
+        			"/check-token").permitAll()
         	.anyRequest()
-        	.authenticated()
+        		.authenticated()
         .and()
         	.oauth2Login();
+        	//.oauth2Client()
+  	    //.and()
+  	    //	.formLogin(); // Add this to use a form login for authenticating
         
     }
 
+    // TODO let the following configure and passwordEncoder methods here to add formLogin in the future.
+    @Override
+    protected void configure(final AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+          .withUser("user1").password(passwordEncoder().encode("user1")).roles("USER")
+          .and()
+          .withUser("admin").password(passwordEncoder().encode("admin")).roles("ADMIN");
+    }
+    
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
--- src/main/java/com/xio91/bots/controllers/CallbackController.java
+++ src/main/java/com/xio91/bots/controllers/CallbackController.java
@@ -1,23 +1,82 @@
 package com.xio91.bots.controllers;
 
+import java.security.Principal;
+
+import javax.servlet.http.HttpSession;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.security.oauth2.client.OAuth2ClientProperties;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.RequestContextHolder;
 
 @RestController
 public class CallbackController {
 
-	@Autowired
-	private OAuth2ClientProperties oauth2ClientProperties;
+	private static final Logger LOG = LoggerFactory.getLogger(CallbackController.class);
 	
-	@Autowired
-	private OAuth2AuthorizedClientService service; 
+    @Autowired
+    private OAuth2AuthorizedClientService authorizedClientService;
 	
+   // @Autowired
+   // private HttpSessionOAuth2AuthorizedClientRepository repo;
+    
+    
 	@GetMapping("/")
 	public String root() {
-		return "root page";
-	}
+ 
 
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		
+		LOG.info("Username (Principal): "+auth.getPrincipal());
+		LOG.info("Credentials: "+auth.getCredentials());
+		LOG.info("Roles: "+ auth.getAuthorities());
+		LOG.info("Details: " + auth.getDetails());
+		
+		SecurityContext securityContext = SecurityContextHolder.getContext();
+	    OAuth2AuthenticationToken oauth2Token = (OAuth2AuthenticationToken) securityContext.getAuthentication();
+	    		
+		OAuth2AuthorizedClient client = authorizedClientService.loadAuthorizedClient(oauth2Token.getAuthorizedClientRegistrationId(), 
+				oauth2Token.getName());
+
+		LOG.info("Access token: "+ client.getAccessToken().getTokenValue());
+		LOG.info("Refresh token: " + client.getRefreshToken().getTokenValue());
+		
+
+		return auth.getPrincipal().toString();
+	}
+	
+	@GetMapping("/login-success")
+	public String root(Principal principal, HttpSession session, OAuth2AuthenticationToken authentication) {
+		 
+		// TODO tratar de ver si se puede recuperar el token de alguna manera para usarlo donde se necesite.
+		//repo.loadAuthorizedClient(clientRegistrationId, principal, request)
+		 //return this.authorizedClientService.loadAuthorizedClient(authentication.getAuthorizedClientRegistrationId(),
+	     //           authentication.getName()).getAccessToken().getTokenValue();
+		
+		LOG.info("parameter principal: "+principal);
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		
+		LOG.info("Username (Principal): "+auth.getPrincipal());
+		LOG.info("Credentials: "+auth.getCredentials());
+		LOG.info("Roles: "+ auth.getAuthorities());
+		LOG.info("Details: " + auth.getDetails());
+		
+		LOG.info("SessionId:" + RequestContextHolder.currentRequestAttributes().getSessionId());
+		LOG.info("SessionId (parameter): " + session.getId());
+		OAuth2AuthorizedClient client = authorizedClientService
+			      .loadAuthorizedClient(
+			        authentication.getAuthorizedClientRegistrationId(), 
+			          authentication.getName());
+
+		LOG.info("Access Token: " +client.getAccessToken());
+		return auth.getPrincipal().toString();
+	}
 }
